918f6f3e2455dbd1230b1dda56b0aa53240f250f
pixelbumper <PixelBumper@users.noreply.github.com>
2019-01-26 23:08:12 +0100

    reverting accidental debug changes

--- Client/Assets/GGJ19/Scripts/Nodes/SendRoleChosenMessage.cs
+++ Client/Assets/GGJ19/Scripts/Nodes/SendRoleChosenMessage.cs
@@ -1,6 +1,5 @@
-﻿using GGJ19.Scripts.GameLogic;
+﻿﻿using GGJ19.Scripts.GameLogic;
 using GGJ19.Scripts.Server_Api;
-using UnityEngine;
 using XNode;
 
 [CreateNodeMenu(nameof(SendRoleChosenMessage), "Request", "Send", "Role")]
@@ -18,17 +17,14 @@ public class SendRoleChosenMessage : FlowNode {
     }
 
     private async void SendServerRequest(string currentRoleChosen) {
-        Debug.Log("Role choosen");
-        
         var serverApi = ServerApi.Instance;
-        var joinRoomAsync = await serverApi.SetRoleAsync("", "", currentRoleChosen);
         var roomName = GameLogicManager.instance.serverRoomName?.Value;
         var playerId = GameLogicManager.instance.PlayerId;
         if (string.IsNullOrEmpty(roomName)) {
             UnityEngine.Debug.LogError("Room name is empty, wont set role on serverside");
             return;
         }
-        //var joinRoomAsync = await serverApi.SetRoleAsync(roomName, playerId, currentRoleChosen);
+        var joinRoomAsync = await serverApi.SetRoleAsync(roomName, playerId, currentRoleChosen);
         GameLogicManager.instance.UpdateGameState(joinRoomAsync.Playing, joinRoomAsync.Waiting);
     }
 }
