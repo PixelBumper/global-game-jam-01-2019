8e066a39ec3baa28e4f67ae45c4ae7c7e115a31c
Luis Rojas <luis.andres81@gmail.com>
2019-01-26 01:37:04 +0100

    added save utils

---
+++ Client/Assets/Plugins/halfblind.saveutils/Scripts/ScriptablePlayerPrefSave.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace HalfBlind.SaveUtils {
+    [CreateAssetMenu(menuName = "Save/PlayerPrefSaver", fileName = "PlayerPrefSaver")]
+    public class ScriptablePlayerPrefSave : ScriptableObject, ISave {
+        public bool Load<T>(string key, out T outValue) {
+            var stringValue = PlayerPrefs.GetString(key, string.Empty);
+            outValue = JsonUtility.FromJson<T>(stringValue);
+            return true;
+        }
+
+        public void Save<T>(string key, T value) {
+            var serializedValue = JsonUtility.ToJson(value);
+            PlayerPrefs.SetString(key, serializedValue);
+        }
+    }
+}
