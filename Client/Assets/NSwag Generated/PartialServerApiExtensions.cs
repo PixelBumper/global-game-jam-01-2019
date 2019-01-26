3ea5a8519cb7daa5184c3d8bd540877ecd561e94
pixelbumper <PixelBumper@users.noreply.github.com>
2019-01-26 23:02:47 +0100

    add missing files

---
+++ Client/Assets/NSwag Generated/PartialServerApiExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GeneratedServerAPI
+{
+    public partial class Client
+    {
+        partial void PrepareRequest(HttpClient client, HttpRequestMessage request, string url)
+        {
+            var requestInfo = $"{request.Method} {request.RequestUri}\n";
+            Debug.Log(requestInfo);
+        }
+
+        partial void PrepareRequest(HttpClient client, HttpRequestMessage request, StringBuilder urlBuilder)
+        {
+            var requestInfo = $"{request.Method} {request.RequestUri}\n";
+            Debug.Log(requestInfo);
+        }
+
+        partial void ProcessResponse(HttpClient client, HttpResponseMessage response)
+        {
+            LogResponse(response.Content.ReadAsStringAsync());
+        }
+
+        private async void LogResponse(Task<string> readAsStringAsync)
+        {
+            try
+            {
+                Debug.Log(await readAsStringAsync);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+            }
+        }
+    }
+}
