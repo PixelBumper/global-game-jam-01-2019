a3aa8c65b31263d64905c5402dee6b6b49e8b236
Luis Rojas <luis.andres81@gmail.com>
2019-01-26 20:08:05 +0100

    Fix issue where playing main scene without the flow was nre every second

--- Client/Assets/GGJ19/Scripts/Nodes/SendRoleChosenMessage.cs
+++ Client/Assets/GGJ19/Scripts/Nodes/SendRoleChosenMessage.cs
@@ -3,26 +3,27 @@ using GGJ19.Scripts.Server_Api;
 using XNode;
 
 [CreateNodeMenu(nameof(SendRoleChosenMessage), "Request", "Send", "Role")]
-public class SendRoleChosenMessage : FlowNode
-{
+public class SendRoleChosenMessage : FlowNode {
     [Input]
     public string roleChosen;
-    
-    public override object GetValue(NodePort port)
-    {
+
+    public override object GetValue(NodePort port) {
         return null;
     }
 
-    public override void ExecuteNode()
-    {
+    public override void ExecuteNode() {
         var currentRoleChosen = GetInputValue(nameof(roleChosen), roleChosen);
         SendServerRequest(currentRoleChosen);
     }
 
-    private async void SendServerRequest(string currentRoleChosen)
-    {
+    private async void SendServerRequest(string currentRoleChosen) {
         var serverApi = ServerApi.Instance;
-        var joinRoomAsync = await serverApi.SetRoleAsync(GameLogicManager.instance.serverRoomName.Value, GameLogicManager.instance.PlayerId, currentRoleChosen);
+        var roomName = GameLogicManager.instance.serverRoomName?.Value;
+        var playerId = GameLogicManager.instance.PlayerId;
+        if (string.IsNullOrEmpty(roomName)) {
+            return;
+        }
+        var joinRoomAsync = await serverApi.SetRoleAsync(roomName, playerId, currentRoleChosen);
         GameLogicManager.instance.UpdateGameState(joinRoomAsync.Playing, joinRoomAsync.Waiting);
     }
 }
