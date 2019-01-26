60780f771eae57866bff8118df9da618c37333fb
Sergio R. Lumley <lumley256@gmail.com>
2019-01-26 15:48:47 +0100

    Added initial flow for reading server data and state

---
+++ Client/Assets/GGJ19/Scripts/GameLogic/GameLogicManager.cs
@@ -0,0 +1,58 @@
+using System.Collections.Specialized;
+using GeneratedServerAPI;
+using HalfBlind.ScriptableVariables;
+using UnityEditor;
+using UnityEngine;
+
+namespace GGJ19.Scripts.GameLogic
+{
+    [CreateAssetMenu(fileName = nameof(GameLogicManager), menuName = "Tools/CreateGameLogicManager")]
+    public class GameLogicManager : ScriptableSingleton<GameLogicManager>
+    {
+        public string PlayerId => SystemInfo.deviceUniqueIdentifier;
+
+        public ScriptableGameEvent onPlayerCountChanged;
+        public ScriptableGameEvent onEmojisChanged;
+        public ScriptableGameEvent onSelectableRoleDisabledChanged;
+        public ScriptableGameEvent onThreatsChanged;
+        public ScriptableGameEvent onTurnChanged;
+        public ScriptableGameEvent onGameWon;
+        public ScriptableGameEvent onGameLost;
+
+        public ScriptableGameEvent onRoomInfoChanged;
+
+        private Playing previousPlayingState;
+        private Room previousRoomState;
+        private Playing currentPlayingState;
+        private Room currentRoomState;
+
+        public void UpdateGameState(Playing playingState, Room roomState)
+        {
+            previousPlayingState = currentPlayingState;
+            currentPlayingState = playingState;
+
+            previousRoomState = currentRoomState;
+            currentRoomState = roomState;
+
+            if (previousPlayingState == null)
+            {
+                onPlayerCountChanged.SendEvent();
+                onEmojisChanged.SendEvent();
+                onSelectableRoleDisabledChanged.SendEvent();
+                onThreatsChanged.SendEvent();
+                onTurnChanged.SendEvent();
+                onGameWon.SendEvent();
+                onGameLost.SendEvent();
+            }
+            
+            if (previousRoomState != currentRoomState
+                || previousRoomState.Name != currentRoomState.Name
+                || previousRoomState.Owner != currentRoomState.Name
+                || previousRoomState.Players.Count != currentRoomState.Players.Count
+                || previousRoomState.PossibleThreats.Count != currentRoomState.PossibleThreats.Count)
+            {
+                onRoomInfoChanged.SendEvent();
+            }
+        }
+    }
+}
---
+++ Client/Assets/GGJ19/Scripts/Nodes/SendRoleChosenMessage.cs
@@ -0,0 +1,28 @@
+﻿using GGJ19.Scripts.GameLogic;
+using GGJ19.Scripts.Server_Api;
+using XNode;
+
+[CreateNodeMenu(nameof(SendRoleChosenMessage), "Request", "Send", "Role")]
+public class SendRoleChosenMessage : FlowNode
+{
+    [Input]
+    public string roleChosen;
+    
+    public override object GetValue(NodePort port)
+    {
+        return null;
+    }
+
+    public override void ExecuteNode()
+    {
+        var currentRoleChosen = GetInputValue(nameof(roleChosen), roleChosen);
+        SendServerRequest(currentRoleChosen);
+    }
+
+    private async void SendServerRequest(string currentRoleChosen)
+    {
+        var serverApi = ServerApi.Instance;
+        var joinRoomAsync = await serverApi.JoinRoomAsync(currentRoleChosen, "1234");
+        GameLogicManager.instance.UpdateGameState(joinRoomAsync.Playing, joinRoomAsync.Waiting);
+    }
+}
