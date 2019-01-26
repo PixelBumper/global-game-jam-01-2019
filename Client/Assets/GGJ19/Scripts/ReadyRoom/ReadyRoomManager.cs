53492d380099258a37e6eca640b3f1284baf64a2
Andy Michael <andy.michael@freiheit.com>
2019-01-26 15:11:36 +0100

    Update ReadyRoom lobby. Press A to simulate an update

---
+++ Client/Assets/GGJ19/Scripts/ReadyRoom/ReadyRoomManager.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using HalfBlind.ScriptableVariables;
+
+public class ReadyRoomManager : MonoBehaviour
+{
+    // A Server has been Created/Joined
+    // Constantly query the server : Do we have 4 players?
+
+    // Public Objects
+    public Text roomIdDisplay;
+    public GameObject playerDisplayContianer;
+
+    // Global Variables
+    public GlobalString _serverRoomName;
+    public GlobalString _myPlayerId;
+
+    // Internal
+    [SerializeField]
+    private ReadyRoomPlayerDisplay[] playerDisplays;
+
+    // From Server
+    private string[] _knownPlayerIds;
+
+
+    private void Awake()
+    {
+        // Set Room Text
+        roomIdDisplay.text = "Room ID: " + _serverRoomName.GetValue();
+        initKnownPlayers();
+        initPlayerDisplays();
+
+        FAKE_SetVarsFromServer();
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            roomInformationResponse();
+        }
+    }
+
+    private void FAKE_SetVarsFromServer()
+    {
+        _myPlayerId.Value = "Andy";
+        _serverRoomName.Value = "Ga4CxP";
+        _knownPlayerIds = new string[] { "", "Andy", "", "Frank" };
+    }
+
+    private void initPlayerDisplays()
+    {
+        playerDisplays = playerDisplayContianer.GetComponentsInChildren<ReadyRoomPlayerDisplay>();
+    }
+
+    private void initKnownPlayers()
+    {
+        _knownPlayerIds = new string[4] { "", "", "", "" };
+    }
+
+
+    // Update Internal
+    private void updatePlayerIds()
+    {
+        for (int i = 0; i < playerDisplays.Length; i++)
+        {
+            playerDisplays[i].setPlayerId(_knownPlayerIds[i], _knownPlayerIds[i] == _myPlayerId.Value);
+        }
+    }
+
+    // Client-Server Communication
+
+    private void roomInformationRequest()
+    {
+
+    }
+
+    private void roomInformationResponse()
+    {
+
+        // Dispatch Updates
+        updatePlayerIds();
+    }
+
+    private void startGame()
+    {
+
+    }
+
+}
--- Client/Assets/GGJ19/Scripts/Server Api/ServerAPI.cs
+++ Client/Assets/GGJ19/Scripts/Server Api/ServerAPI.cs
@@ -6,7 +6,7 @@ namespace GGJ19.Scripts.Server_Api
     public class ServerApi :Client
     {
         private static readonly HttpClient HttpClient = new HttpClient();
-        public static ServerApi Instance { get; } = new ServerApi("http://localhost:8080", HttpClient);
+        public static ServerApi Instance { get; } = new ServerApi("https://ggj19.herokuapp.com", HttpClient);
 
         private ServerApi(string baseUrl, HttpClient httpClient) : base(baseUrl, httpClient)
         {
