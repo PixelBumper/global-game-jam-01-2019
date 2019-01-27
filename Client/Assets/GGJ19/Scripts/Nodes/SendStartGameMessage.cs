2576510685d96f8c46dad718ebdd28140ebd0c09
exAntares <luis.andres81@gmail.com>
2019-01-27 02:00:14 +0100

    Added log to response on StartGame

--- Client/Assets/GGJ19/Scripts/Nodes/SendStartGameMessage.cs
+++ Client/Assets/GGJ19/Scripts/Nodes/SendStartGameMessage.cs
@@ -6,32 +6,27 @@ using UnityEngine;
 using XNode;
 
 [CreateNodeMenu(nameof(SendStartGameMessage), "Request", "Start", "Room", "Game")]
-public class SendStartGameMessage : FlowNode
-{
+public class SendStartGameMessage : FlowNode {
 
-    public override object GetValue(NodePort port)
-    {
+    public override object GetValue(NodePort port) {
         return null;
     }
 
-    public override void ExecuteNode()
-    {
+    public override void ExecuteNode() {
         string roomName = GameLogicManager.instance.serverRoomName.Value;
         string playerId = GameLogicManager.instance.MyPlayerId;
         SendServerRequest(roomName, playerId);
     }
 
-    private async Task SendServerRequest(string roomName, string playerId)
-    {
+    private async Task SendServerRequest(string roomName, string playerId) {
         Debug.Log("Sending StartGame Request: " + roomName + " : " + playerId);
-        if (!string.IsNullOrEmpty(roomName) && !string.IsNullOrEmpty(playerId))
-        {
+        if (!string.IsNullOrEmpty(roomName) && !string.IsNullOrEmpty(playerId)) {
             // No Nulls in request room!
             Debug.Log("Valid. Sent.");
-
             var serverApi = ServerApi.Instance;
-            RoomInformation joinRoomAsync = await serverApi.StartRoomAsync(roomName, playerId);
-            GameLogicManager.instance.UpdateGameState(joinRoomAsync.Playing, joinRoomAsync.Waiting);
+            RoomInformation startRoomResponse = await serverApi.StartRoomAsync(roomName, playerId);
+            Debug.Log($"{nameof(startRoomResponse)}:{startRoomResponse.ToJson()}");
+            GameLogicManager.instance.UpdateGameState(startRoomResponse.Playing, startRoomResponse.Waiting);
         }
     }
 }
